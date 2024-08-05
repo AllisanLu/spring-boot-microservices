3b56b0ed420a91d35f4d394b0121f58f10b0657d
Allison <alcp2610@gmail.com>
2024-08-05 17:44:06 +0000

    part 2

--- spring-boot-microservices/src/main/java/com/example/springbootmicroservices/api/CustomerAPI.java
+++ spring-boot-microservices/src/main/java/com/example/springbootmicroservices/api/CustomerAPI.java
@@ -1,45 +1,45 @@
 package com.example.springbootmicroservices.api;
 import com.example.springbootmicroservices.data.Customer;
-import com.example.springbootmicroservices.data.CustomersRepository;
+import com.example.springbootmicroservices.data.CustomerRespository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Optional;
 @RestController
-@RequestMapping("/customers")
+@RequestMapping("/api")
 public class CustomerAPI {
 	@Autowired
-	private CustomersRepository customerRepository;
+	private CustomerRespository customerRepository;
 	
-	@GetMapping
-	public List<Customer> getAll() {
+	@GetMapping("/customers")
+	public Iterable<Customer> getAll() {
 		return customerRepository.findAll();
 	}
 	
-	@GetMapping("/{customerId}")
+	@GetMapping("/customers/{customerId}")
 	public Optional<Customer> getCustomerById(@PathVariable("customerId") Long id) {
 		return customerRepository.findById(id);
 	}
 	
-	@PostMapping
-	public Customer createCustomer(@RequestBody Customer customer) {
-		return customerRepository.save(customer);
-	}
-	
-	@PutMapping("/{customerId}")
-	public Optional<Customer> updateCustomer(@PathVariable("customerId") Long id, @RequestBody Customer customerDetails) {
-		Optional<Customer> customerOpt = customerRepository.findById(id);
-		if (customerOpt.isPresent()) {
-			Customer customer = customerOpt.get();
-			customer.setName(customerDetails.getName());
-			customer.setEmail(customerDetails.getEmail());
-			return Optional.of(customerRepository.save(customer));
-		}
-		return Optional.empty();
-	}
-	
-	@DeleteMapping("/{customerId}")
-	public void deleteCustomer(@PathVariable("customerId") Long id) {
-		customerRepository.deleteById(id);
-	}
+//	@PostMapping
+//	public Customer createCustomer(@RequestBody Customer customer) {
+//		return customerRepository.save(customer);
+//	}
+//	
+//	@PutMapping("/{customerId}")
+//	public Optional<Customer> updateCustomer(@PathVariable("customerId") Long id, @RequestBody Customer customerDetails) {
+//		Optional<Customer> customerOpt = customerRepository.findById(id);
+//		if (customerOpt.isPresent()) {
+//			Customer customer = customerOpt.get();
+//			customer.setName(customerDetails.getName());
+//			customer.setEmail(customerDetails.getEmail());
+//			return Optional.of(customerRepository.save(customer));
+//		}
+//		return Optional.empty();
+//	}
+//	
+//	@DeleteMapping("/{customerId}")
+//	public void deleteCustomer(@PathVariable("customerId") Long id) {
+//		customerRepository.deleteById(id);
+//	}
 }
