5fc41ce1d201ef339db32dccbaa8d312d7cc71c2
Allison <alcp2610@gmail.com>
2024-08-06 14:45:08 +0000

    Fix application load test

--- spring-boot-microservices/src/test/java/com/example/spring_boot_microservices/SpringBootMicroservicesApplicationTests.java
+++ spring-boot-microservices/src/test/java/com/example/spring_boot_microservices/SpringBootMicroservicesApplicationTests.java
@@ -3,7 +3,9 @@ package com.example.spring_boot_microservices;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import com.example.springbootmicroservices.App;
+
+@SpringBootTest(classes=App.class)
 class SpringBootMicroservicesApplicationTests {
 
 	@Test
